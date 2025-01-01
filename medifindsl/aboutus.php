8b95da13020cb5adc4494af32c6bd01b4b5796f1
Supun Sulakshana <132369603+supuns2001@users.noreply.github.com>
2025-01-01 23:44:58 +0530

    last update

--- medifindsl/aboutus.php
+++ medifindsl/aboutus.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us | MEDIFINDSL  Test 333</title>
+    <title>About Us | MEDIFINDSL </title>
 
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="bootstrap.css" />
